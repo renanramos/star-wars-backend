d18d731c1e4b265c58896c1415d6f1d13654078a
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-07-09 15:39:29 -0300

    Corrigido nome do campo user_name e nome da tabela (my-db)

--- consts/consts.js
+++ consts/consts.js
@@ -1,4 +1,4 @@
-const _login_sql = `SELECT user.user_id, user.user_name FROM \`sw-portal\`.\`user\` WHERE user.user_name = ? and user.user_password = ?`;
+const _login_sql = `SELECT user.user_id, user.user_name FROM \`my-db\`.\`user\` WHERE user.user_name = ? and user.user_password = ?`;
 
 module.exports = {
     login_sql: _login_sql
